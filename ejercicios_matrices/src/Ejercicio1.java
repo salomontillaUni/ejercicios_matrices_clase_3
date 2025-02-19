470d99e3d357b05facd591933333c17dca7e1476
Salomon Montilla <thebilystaxx@gmail.com>
2025-02-19 08:53:05 -0500

    agrega archivos

---
+++ ejercicios_matrices/src/Ejercicio1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int matriz[][] = new int [3][3];
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.println("Ingresa un numero para la posicion " + i + ", " + j );
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print(matriz[i][j] + " " );
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ ejercicios_matrices/src/Ejercicio2.java
@@ -0,0 +1,13 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int matriz[][] = new int [3][3];
+        int matriz2[][] = new int [3][3];
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+               
+            }
+            System.out.println();
+        }
+    }
+}
