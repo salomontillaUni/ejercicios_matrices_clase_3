b564a799aacf0cf9151af55c310a6b3fbc51cb01
salo <salomontilla@outlook.com>
2025-02-19 16:35:25 -0500

    corrige orden de los ejercicios

--- ejercicios_matrices/src/Ejercicio4.java
+++ ejercicios_matrices/src/Ejercicio4.java
@@ -1,5 +1,12 @@
 public class Ejercicio4 {
     public static void main(String[] args) {
-
+        int[][] matriz = {{1,1}, {1,1}};
+        System.out.println("Matriz multiplicada por 2: ");
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 2; j++){
+                System.out.print(matriz[i][j] * 2  + " ");
+            }
+            System.out.println();
+        }
     }
 }
--- ejercicios_matrices/src/Ejercicio5.java
+++ ejercicios_matrices/src/Ejercicio5.java
@@ -1,11 +1,12 @@
 public class Ejercicio5 {
     public static void main(String[] args) {
-        int[][] matriz = {{1,1}, {1,1}};
-        for(int i = 0; i < 2; i++){
-            for(int j = 0; j < 2; j++){
-                System.out.print(matriz[i][j] * 2  + " ");
+        int[][] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int resultado = 0;
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                resultado += matriz[i][j];
             }
-            System.out.println();
         }
+        System.out.println("La suma de los elementos de la matriz es: " + resultado);
     }
 }
