986d9aec24ff53b1d833951dfc650a2fdd5903ef
salo <salomontilla@outlook.com>
2025-02-19 16:33:37 -0500

    suma de los elementos de la matriz terminada

--- ejercicios_matrices/src/Ejercicio6.java
+++ ejercicios_matrices/src/Ejercicio6.java
@@ -1,5 +1,12 @@
 public class Ejercicio6 {
     public static void main(String[] args) {
-
+        int[][] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int resultado = 0;
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                resultado += matriz[i][j];
+            }
+        }
+        System.out.println("La suma de los elementos de la matriz es: " + resultado);
     }
 }
