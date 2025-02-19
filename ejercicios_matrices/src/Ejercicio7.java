550c6d4a9a6ef7fa2897b59697744c6f79ee7797
salo <salomontilla@outlook.com>
2025-02-19 16:43:07 -0500

    suma de matrices terminado

--- ejercicios_matrices/src/Ejercicio7.java
+++ ejercicios_matrices/src/Ejercicio7.java
@@ -1,5 +1,28 @@
 public class Ejercicio7 {
     public static void main(String[] args) {
+        int[][] matriz = {{1, 2},
+                {3, 4}};
+        int[][] matriz2 = {{5, 6},
+                {7, 8}};
 
+        int[][] resultado = sumaMatrices(matriz, matriz2);
+        System.out.println("Suma de matrices: ");
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 2; j++){
+                System.out.print(resultado[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
+
+    public static int[][] sumaMatrices(int[][] matriz1, int[][] matriz2){
+        int[][] matrizSuma = new int[matriz1.length][matriz1[0].length];
+        for(int i = 0; i < matriz1.length; i++){
+            for(int j = 0; j < matriz1[0].length; j++){
+                matrizSuma[i][j] = matriz1[i][j] + matriz2[i][j];
+            }
+        }
+        return matrizSuma;
+    }
+
 }
